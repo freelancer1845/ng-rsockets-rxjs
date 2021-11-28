7a47cfb2e82e50cb5f845649ba15b8ccca8d4997
Jascha Riedel <jaschelite@googlemail.com>
2021-11-28 12:52:03 +0100

    Version 0.0.13 (fix for automatic reconnect)
    rsocket-rxjs update to 0.1.3
    
    Signed-off-by: Jascha Riedel <jaschelite@googlemail.com>

--- angular-tester/src/app/app.component.ts
+++ angular-tester/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RSocketService } from 'ng-rsocket-rxjs';
+import { repeat, tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,26 @@ import { RSocketService } from 'ng-rsocket-rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'angular-tester';
+  counter = 0;
 
   constructor(private service: RSocketService) {
   }
 
   ngOnInit(): void {
     this.service.route('/basic/request-response').data("Hello World").requestResponse().subscribe(ans => this.title = ans);
+    this.service.route('/basic/request-stream/unending').requestStream()
+      .pipe(
+        tap({
+          next: n => console.log("Next Unending", n),
+          error: err => console.log("Error in Unending", err),
+          complete: () => console.log("Unending completed")
+        }),
+        repeat()
+      )
+      .subscribe(ans => this.counter++);
+  }
+
+  closeConnectionByRemote() {
+    this.service.route('/basic/disconnect').fireAndForget();
   }
 }
